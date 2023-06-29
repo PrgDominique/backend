3d917b9755a526caf3ea8137ca9ed322820e1762
PrgDominique <dominiquesimbillo10@gmail.com>
2023-06-29 19:32:54 +0800

    added dotenv and mongodb package

--- GraphQL/src/index.ts
+++ GraphQL/src/index.ts
@@ -2,37 +2,86 @@ import { ApolloServer } from '@apollo/server'
 import { startStandaloneServer } from '@apollo/server/standalone'
 
 // Resolvers define how to fetch the types defined in your schema.
-// This resolver retrieves books from the "books" array above.
-const typeDefs = `#graphql
-    type Book {
-        title: String
-        author: String
-    }
+// This resolver retrieves books from the "books" array below.
 
-    type Query {
-        books: [Book]
-    }
-`
-const books = [
+const libraries = [
   {
-    title: 'harry potter',
-    author: 'jopay',
+    branch: 'downtown',
   },
+  {
+    branch: 'riverside',
+  },
+]
+
+const books = [
   {
     title: 'The Awakening',
     author: 'Kate Chopin',
+    branch: 'riverside',
   },
   {
     title: 'City of Glass',
     author: 'Paul Auster',
+    branch: 'downtown',
   },
 ]
+const typeDefs = `#graphql
 
+   type Library {
+      id: id
+      branch: String!
+      books: [Book!]
+    }
+
+    type Book {
+        title: String!
+        author: Author!
+    }
+    type Author {
+        name: String!
+    }
+
+    type Query {
+        libraries: [Library]
+    }
+
+`
+
+
+
+// Resolver map
 const resolvers = {
   Query: {
-    books: () => books,
+    libraries() {
+      // Return our hardcoded array of libraries
+      return libraries
+    },
   },
+  Library: {
+    books(parent) {
+      // Filter the hardcoded array of books to only include
+      // books that are located at the correct branch
+      return books.filter((book) => book.branch === parent.branch)
+    },
+  },
+  Book: {
+    // The parent resolver (Library.books) returns an object with the
+    // author's name in the "author" field. Return a JSON object containing
+    // the name, because this field expects an object.
+    author(parent) {
+      return {
+        name: parent.author,
+      }
+    },
+  },
+
+  // Because Book.author returns an object with a "name" field,
+  // Apollo Server's default resolver for Author.name will work.
+  // We don't need to define one.
 }
+
+// Pass schema definition and resolvers to the
+// ApolloServer constructor
 
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
